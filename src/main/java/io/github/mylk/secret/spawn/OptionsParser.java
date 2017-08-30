a9294f74da9696367a171c165c95bfa2c2da1eb1
Kostas Milonas <milonas.ko@gmail.com>
2017-08-31 00:27:46 +0300

    creates OptionsParser to handle both configuration file and cli options

---
+++ src/main/java/io/github/mylk/secret/spawn/OptionsParser.java
@@ -0,0 +1,62 @@
+package io.github.mylk.secret.spawn;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.apache.commons.cli.CommandLine;
+
+public class OptionsParser
+{
+    private final String PROPERTY_FILENAME = "config.properties";
+
+    private final static Properties FILE_OPTIONS = new Properties();
+    private final CommandLine CLI_OPTIONS;
+
+    public OptionsParser(CommandLine options)
+    {
+        if (FILE_OPTIONS.isEmpty()) {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PROPERTY_FILENAME);
+            if (inputStream != null) {
+                try {
+                    FILE_OPTIONS.load(inputStream);
+                    inputStream.close();
+                } catch (IOException ex) {
+                }
+            } else {
+                System.out.println("File '" + PROPERTY_FILENAME + "' not found.");
+            }
+        }
+
+        CLI_OPTIONS = options;
+    }
+
+    public String getFileOptionValue(String property)
+    {
+        String value = "";
+
+        try {
+            value = FILE_OPTIONS.getProperty(property);
+        } catch (Exception ex) {
+        }
+
+        return value;
+    }
+
+    public String getCliOptionValue(String property)
+    {
+        String value = "";
+
+        if (CLI_OPTIONS.hasOption(property)) {
+            value = CLI_OPTIONS.getOptionValue(property);
+        }
+
+        return value;
+    }
+
+    public String getOption(String fileOptionPropertyName, String cliOptionPropertyName)
+    {
+        String fileOptionValue = getFileOptionValue(fileOptionPropertyName);
+        String cliOptionValue = getCliOptionValue(cliOptionPropertyName);
+        return !cliOptionValue.isEmpty() ? cliOptionValue : fileOptionValue;
+    }
+}
--- src/main/java/io/github/mylk/secret/spawn/SecretSpawn.java
+++ src/main/java/io/github/mylk/secret/spawn/SecretSpawn.java
@@ -1,42 +1,37 @@
 package io.github.mylk.secret.spawn;
 
+import org.apache.commons.cli.*;
 import io.github.mylk.secret.spawn.parser.Wikipedia;
 import io.github.mylk.secret.spawn.client.Rest;
-import org.apache.commons.cli.*;
 
 public class SecretSpawn
 {
     public static void main(String[] args)
     {
         // defining the options
-        Options options = new Options();
+        Options cliOptions = new Options();
         CommandLine cmd = null;
-        options.addOption("length", true, "The secret's length");
+        cliOptions.addOption("length", true, "The secret's length");
 
         // parse command options, show help when failing
         CommandLineParser optionParser = new DefaultParser();
         try {
-            cmd = optionParser.parse(options, args);
+            cmd = optionParser.parse(cliOptions, args);
         } catch (ParseException ex) {
             HelpFormatter helpFormatter = new HelpFormatter();
-            helpFormatter.printHelp("secret-spawn", options);
+            helpFormatter.printHelp("secret-spawn", cliOptions);
             System.out.println(ex.getMessage());
             System.exit(1);
         }
 
-        // getting config options (command options or configuration)
-        String url = "";
-        Integer secretLength = null;
-        ConfigParser config = new ConfigParser();
-        try {
-            url = config.get("source.url.wikipedia");
-            secretLength = Integer.parseInt(config.get("secret.length"));
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        // getting config options (cli or configuration file options)
+        OptionsParser options = new OptionsParser(cmd);
+        Integer secretLength = Integer.parseInt(options.getOption("secret.length", "length"));
+
+        String url = options.getFileOptionValue("source.url.wikipedia");
+        if (url.isEmpty()) {
+            System.out.println("The URL of the phrase source could not be found.");
             System.exit(1);
-        }
-        if (cmd.hasOption("length")) {
-            secretLength = Integer.parseInt(cmd.getOptionValue("length"));
         }
 
         // calling the source
@@ -45,7 +40,7 @@ public class SecretSpawn
         try {
             response = client.get(url);
         } catch (Exception ex) {
-            System.out.println("Something went wrong with the call to the phrase source: " + ex.getMessage());
+            System.out.println("The call to the phrase source failed: " + ex.getMessage());
             System.exit(1);
             return;
         }
